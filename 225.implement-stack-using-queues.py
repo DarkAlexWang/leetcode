d9934d6921cf128a9646662f315759accea33f6c
Alex Wang <wangzhihuan0815@gmail.com>
2020-01-10 12:18:01 -0800

    add 225

---
+++ 225.implement-stack-using-queues.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=225 lang=python3
+#
+# [225] Implement Stack using Queues
+#
+
+# @lc code=start
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+# @lc code=end
