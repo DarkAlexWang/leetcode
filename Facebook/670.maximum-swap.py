b48577406ec0e0ac195a8162c0c091cc62197add
Alex Wang <wangzhihuan0815@gmail.com>
2020-11-26 11:54:16 -0800

    MBP 11/26

---
+++ Facebook/670.maximum-swap.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=670 lang=python3
+#
+# [670] Maximum Swap
+#
+
+# @lc code=start
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        arry = list(str(num))
+        last = {int(x): i for i, x in enumerate(arry)}
+        print(last)
+        for i, x in enumerate(arry):
+            for d in range(9, int(x), -1):
+                if last.get(d, 0) > i:
+                    arry[i], arry[last[d]] = arry[last[d]], arry[i]
+                    return int("".join(arry))
+        return num
+
+
+# @lc code=end
