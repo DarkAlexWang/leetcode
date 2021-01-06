5d6c78302337028f85fc2b5a77cc679f1960daa5
Alex Wang <wangzhihuan0815@gmail.com>
2021-01-06 10:53:32 -0800

    MP 1-7

---
+++ Facebook/224.basic-calculator.py
@@ -0,0 +1,21 @@
+# @lc lang=python3
+class Solution:
+    def calculate(self, s: str) -> int:
+        res, num, sign, stack = 0, 0, 1, []
+        for ss in s:
+            if ss.isdigit():
+                nums = 10 * num + int(ss)
+            elif ss in ['+', '-']:
+                res += sign * num
+                num = 0
+                sign = 1 if ss == '+' else -1
+            elif ss == "(":
+                stack.append(res)
+                stack.append(sign)
+                sign, res = 1, 0
+            elif ss == ")":
+                res += sign * num
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+        return res + num * sign
